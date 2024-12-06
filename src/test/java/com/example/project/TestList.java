5e31220549e9484e4cd516bbb5a0edcb4398c817
AbdullahFasial <xqabdullhqx@gmail.com>
2024-12-06 09:11:14 +0300

    Update TestList.java

--- src/test/java/com/example/project/TestList.java
+++ src/test/java/com/example/project/TestList.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class TestList {
+public class TestList { 
 
    @Test
    public void testMostFrequentElement1()
